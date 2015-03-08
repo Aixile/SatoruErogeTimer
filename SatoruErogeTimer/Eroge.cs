a97f263880aeb792be5df93e70328ad9938f893b
Aixile <amane.nagatsuki@gmail.com>
2015-03-08 19:30:30 +0900

    Split Class

---
+++ SatoruErogeTimer/Eroge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SatoruErogeTimer
+{
+	public class Eroge
+	{
+		public string title;
+		public float time;
+		public string addr;
+		public string processName;
+
+	}
+}
---
+++ SatoruErogeTimer/GZipResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SatoruErogeTimer
+{
+	public class GZipResult
+	{
+		public GZipFileInfo[] Files = null;
+		public int FileCount = 0;
+		public long TempFileSize = 0;
+		public long ZipFileSize = 0;
+		public int CompressionPercent = 0;
+		public string TempFile = null;
+		public string ZipFile = null;
+		public bool TempFileDeleted = false;
+		public bool Errors = false;
+
+	}
+}
